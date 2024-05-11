039bd75ac0ed0d6aa3aa912bdbb5a628724b9f91
GhostTalker <49254289+GhostTalker@users.noreply.github.com>
2024-05-11 18:49:29 +0200

    restart modem

--- DongleControl.py
+++ DongleControl.py
@@ -1,83 +1,72 @@
 #!/usr/bin/env /bin/python3
 #
 # DongleControl
-# Script to restart a 4g dongle
+# Script to restart a 4G dongle
 #
 __author__ = "GhostTalker"
 __copyright__ = "Copyright 2023, The GhostTalker project"
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __status__ = "DEV"
 
 import requests
 import schedule
 import time
 import configparser
-import subprocess
+from toggleDataSwitch import toggle_modem
 
+# Globale Variable
+dongle_statuses = {}
 
 def load_configuration():
-  config = configparser.ConfigParser()
-  config.read('config.ini')
-  dongles = {key: config['DONGLES'][key] for key in config['DONGLES']}
-  proxys = {key: config['PROXYS'][key] for key in config['PROXYS']}
-  associations = {key: proxys[config['ASSOCIATIONS'][key]] for key in config['ASSOCIATIONS']}
-  dongle_statuses = {dongle: {'IP': ip, 'Proxy': associations[dongle]} for dongle, ip in dongles.items()}
+    global dongle_statuses
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    dongles = {key: config['DONGLES'][key] for key in config['DONGLES']}
+    proxys = {key: config['PROXYS'][key] for key in config['PROXYS']}
+    associations = {key: proxys[config['ASSOCIATIONS'][key]] for key in config['ASSOCIATIONS']}
+    dongle_statuses = {dongle: {'IP': ip, 'Proxy': associations[dongle]} for dongle, ip in dongles.items()}
 
-  for dongle_id, details in dongle_statuses.items():
-    ext_ip = get_public_ip(details['Proxy'])
-    details['extIP'] = ext_ip if ext_ip else "Unavailable"
-
-  return dongle_statuses
-
+    for dongle_id, details in dongle_statuses.items():
+        ext_ip = get_public_ip(details['Proxy'])
+        details['extIP'] = ext_ip if ext_ip else "Unavailable"
 
 def get_public_ip(proxy):
-  try:
-    proxies = {
-      'http': f'http://{proxy}'
-    }
-    headers = {
-        'User-Agent': 'curl/7.64.1'  # Imitieren des curl User-Agents
-    }
-    response = requests.get('http://api.ipify.org', headers=headers, proxies=proxies, timeout=10)
-    if response.status_code == 200:
-      return response.text.strip()
-    else:
-      print(f"Response returned with status code: {response.status_code}, response: {response.text}")
-      return "Response Error"
-  except requests.RequestException as e:
-    print(f"Failed to get public IP through proxy {proxy}: {e}")
-    return None
+    try:
+        proxies = {
+            'http': f'http://{proxy}'
+        }
+        headers = {
+            'User-Agent': 'curl/7.64.1'
+        }
+        response = requests.get('http://api.ipify.org', headers=headers, proxies=proxies, timeout=10)
+        if response.status_code == 200:
+            return response.text.strip()
+        else:
+            print(f"Response returned with status code: {response.status_code}, response: {response.text}")
+            return "Response Error"
+    except requests.RequestException as e:
+        print(f"Failed to get public IP through proxy {proxy}: {e}")
+        return None
 
+def toggle_dataswitch(dongle_id):
+    global dongle_statuses
+    toggle_modem(dongle_statuses[dongle_id]['IP'], '0')
+    time.sleep(10)
+    toggle_modem(dongle_statuses[dongle_id]['IP'], '1')
+    time.sleep(10)
+    update_extIP(dongle_id)
 
-def call_toggle_script(host, state):
-    # Pfad zur Python-Executable und Skript angeben
-    result = subprocess.run(['python3', 'toggleDataSwitch.py', host, state], capture_output=True, text=True)
-    if result.returncode == 0:
-      print("Toggle script executed successfully.")
-      print(result.stdout)
-    else:
-      print("Toggle script failed.")
-      print(result.stderr)
+def update_extIP(dongle_id):
+    global dongle_statuses
+    ext_ip = get_public_ip(dongle_statuses[dongle_id]['Proxy'])
+    dongle_statuses[dongle_id]['extIP'] = ext_ip if ext_ip else "Unavailable"
 
 def main():
-    dongle_statuses = load_configuration()
-    print(dongle_statuses)
-    for dongle in dongle_statuses:
-        call_toggle_script(dongle_statuses[dongle]['IP'], '0')
-
-    time.sleep(10)
-    for dongle in dongle_statuses:
-        call_toggle_script(dongle_statuses[dongle]['IP'], '1')
-
-        ext_ip = get_public_ip(dongle_statuses[dongle]['Proxy'])
-        dongle_statuses[dongle]['extIP'] = ext_ip if ext_ip else "Unavailable"
-
-    time.sleep(10)
-    for dongle in dongle_statuses:
-        ext_ip = get_public_ip(dongle_statuses[dongle]['Proxy'])
-        dongle_statuses[dongle]['extIP'] = ext_ip if ext_ip else "Unavailable"
-
+    global dongle_statuses
+    load_configuration()
+    for dongle_id in dongle_statuses:
+        toggle_dataswitch(dongle_id)
     print(dongle_statuses)
 
 if __name__ == "__main__":
-  main()
+    main()
--- toggleDataSwitch.py
+++ toggleDataSwitch.py
@@ -1,9 +1,5 @@
-# toggleDataSwitch.py
 import requests
 import xmltodict
-import sys
-import argparse
-
 
 def toggle_modem(host, state):
     BASE_URL = f'http://{host}'
@@ -12,17 +8,13 @@ def toggle_modem(host, state):
 
     session = requests.Session()
     try:
-        # Get session and verification tokens from the modem
         response = session.get(BASE_URL + TOKEN_URL, timeout=3)
         response_dict = xmltodict.parse(response.text).get('response', None)
-
         headers = {
             'Cookie': response_dict['SesInfo'],
             '__RequestVerificationToken': response_dict['TokInfo']
         }
         data = f'<?xml version="1.0" encoding="UTF-8"?><request><dataswitch>{state}</dataswitch></request>'
-
-        # Send the toggle request
         response = session.post(BASE_URL + SWITCH_URL, data=data, headers=headers, timeout=3)
         if response.status_code == 200:
             print(f"Modem at {host} successfully toggled to state {state}.")
@@ -33,12 +25,3 @@ def toggle_modem(host, state):
     except Exception as e:
         print(f"An error occurred: {e}")
         return False
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Toggle a modem's data switch.")
-    parser.add_argument("host", help="The IP address of the modem")
-    parser.add_argument("state", choices=['0', '1'], help="The state to toggle the modem to: '0' for off, '1' for on")
-    args = parser.parse_args()
-
-    toggle_modem(args.host, args.state)
